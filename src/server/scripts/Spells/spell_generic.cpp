9b93d6bdae9717421ec23173edebd3f52ed5b106
Siley <r9.c@seznam.cz>
2014-04-05 11:41:59 +0200

    Shadowmeld fix

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -3701,8 +3701,65 @@ class spell_gen_eject_all_passengers : public SpellScriptLoader
         }
 };
 
+class spell_shadowmeld : public SpellScriptLoader
+{
+public:
+	spell_shadowmeld() : SpellScriptLoader("spell_shadowmeld") {}
+
+	class spell_shadowmeld_SpellScript : public SpellScript
+	{
+		PrepareSpellScript(spell_shadowmeld_SpellScript);
+
+		void HandleDummy(SpellEffIndex /*effIndex*/)
+		{
+			Unit *caster = GetCaster();
+			if (!caster)
+				return;
+
+			caster->InterruptSpell(CURRENT_AUTOREPEAT_SPELL); // break Auto Shot and autohit
+			caster->InterruptSpell(CURRENT_CHANNELED_SPELL);  // break channeled spells
+
+			bool instant_exit = true;
+			if (Player *pCaster = caster->ToPlayer()) // if is a creature instant exits combat, else check if someone in party is in combat in visibility distance
+			{
+				uint64 myGUID = pCaster->GetGUID();
+				float visibilityRange = pCaster->GetMap()->GetVisibilityRange();
+				if (Group *pGroup = pCaster->GetGroup())
+				{
+					const Group::MemberSlotList membersList = pGroup->GetMemberSlots();
+					for (Group::member_citerator itr = membersList.begin(); itr != membersList.end() && instant_exit; ++itr)
+					if (itr->guid != myGUID)
+					if (Player *GroupMember = Unit::GetPlayer(*pCaster, itr->guid))
+					if (GroupMember->IsInCombat() && pCaster->GetMap() == GroupMember->GetMap() && pCaster->IsWithinDistInMap(GroupMember, visibilityRange))
+						instant_exit = false;
+				}
+
+				pCaster->SendAttackSwingCancelAttack();
+			}
+
+			if (!caster->GetInstanceScript() || !caster->GetInstanceScript()->IsEncounterInProgress()) //Don't leave combat if you are in combat with a boss
+			{
+				if (!instant_exit)
+					caster->getHostileRefManager().deleteReferences(); // exit combat after 6 seconds
+				else caster->CombatStop(); // isn't necessary to call AttackStop because is just called in CombatStop
+			}
+		}
+
+		void Register()
+		{
+			OnEffectHitTarget += SpellEffectFn(spell_shadowmeld_SpellScript::HandleDummy, EFFECT_1, SPELL_EFFECT_DUMMY);
+		}
+	};
+
+	SpellScript* GetSpellScript() const
+	{
+		return new spell_shadowmeld_SpellScript();
+	}
+};
+
 void AddSC_generic_spell_scripts()
 {
+	new spell_shadowmeld();
     new spell_gen_absorb0_hitlimit1();
     new spell_gen_adaptive_warding();
     new spell_gen_allow_cast_from_item_only();
