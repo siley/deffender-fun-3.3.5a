a485d8a5c55e26b62d4c53befa65a1e6cbb76b64
Siley <r9.c@seznam.cz>
2014-04-13 20:53:17 +0200

    Trial Of The Champion

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1450,6 +1450,11 @@ void AddSC_npc_argent_ring_restorer();
 void AddSC_command_mmr();
 void AddSC_object_arena_crystal();
 void AddSC_CPWS_Transmogrification();
+void AddSC_TW_instance_trial_of_the_champion();
+void AddSC_TW_boss_grand_champions();
+void AddSC_TW_boss_black_knight();
+void AddSC_TW_boss_argent_challenge();
+void AddSC_TW_trial_of_the_champion();
 #endif
 
 void AddCustomScripts()
@@ -1466,5 +1471,10 @@ void AddCustomScripts()
 	AddSC_command_mmr();
 	AddSC_object_arena_crystal();
 	AddSC_CPWS_Transmogrification();
+	AddSC_TW_instance_trial_of_the_champion();
+	AddSC_TW_boss_grand_champions();
+	AddSC_TW_boss_black_knight();
+	AddSC_TW_boss_argent_challenge();
+	AddSC_TW_trial_of_the_champion();
 #endif
 }
