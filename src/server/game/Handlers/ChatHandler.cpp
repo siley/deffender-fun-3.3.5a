a1762eafe88507f573d527b156943e0148750f5c
Siley <r9.c@seznam.cz>
2014-04-13 20:18:14 +0200

    GM chat blue

--- src/server/game/Handlers/ChatHandler.cpp
+++ src/server/game/Handlers/ChatHandler.cpp
@@ -237,6 +237,9 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
         }
     }
 
+	if (sender->isGMChat() && !msg.empty())
+		msg = "|cff2998ff" + msg + "|r";
+
     switch (type)
     {
         case CHAT_MSG_SAY:
