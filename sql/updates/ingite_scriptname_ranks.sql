850e5030a45fa74a212041f6dfa1faa498d4f8a3
Daniel Bunda <dnlbunda@gmail.com>
2014-04-26 14:58:03 +0200

    Update Ignite SpellScriptNames

---
+++ sql/updates/ingite_scriptname_ranks.sql
@@ -0,0 +1,8 @@
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_mage_ignite';
+INSERT INTO spell_script_names VALUES
+(-11119, 'spell_mage_ignite'),
+(-11120, 'spell_mage_ignite'),
+(-12654, 'spell_mage_ignite'),
+(-12846, 'spell_mage_ignite'),
+(-12847, 'spell_mage_ignite'),
+(-12848, 'spell_mage_ignite');
