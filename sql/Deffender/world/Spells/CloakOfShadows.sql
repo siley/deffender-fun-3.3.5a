97937474ab99ec480a90cba3110a36dbdd24002b
Effec7 <dnlbunda@gmail.com>
2014-07-04 13:51:46 +0200

    Cloak of Shadows should remove Swarming Shadows from now.
    Signed-off-by: Siley <r9.c@seznam.cz>

---
+++ sql/Deffender/world/Spells/CloakOfShadows.sql
@@ -0,0 +1,8 @@
+-- Swarming Shadows removed by CoS
+DELETE FROM spell_linked_spell WHERE spell_effect = -71265;
+INSERT INTO spell_linked_spell VALUES
+(31224,-71265,1,'Removing Swarm - flames part');
+
+DELETE FROM spell_linked_spell WHERE spell_effect = -71264;
+INSERT INTO spell_linked_spell VALUES
+(31224,-71264,1,'Removing Swarm - before flames start part');
