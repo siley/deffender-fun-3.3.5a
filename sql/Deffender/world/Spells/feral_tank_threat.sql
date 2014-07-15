ae246c36c5f5f8dde952fe748079a3ec3ce34bb9
Effec7 <dnlbunda@gmail.com>
2014-07-16 00:21:14 +0200

    Fixed UP Feral Tank (Druid) Spell Threat values
    Signed-off-by: Siley <r9.c@seznam.cz>

---
+++ sql/Deffender/world/Spells/feral_tank_threat.sql
@@ -0,0 +1,9 @@
+DELETE FROM spell_threat WHERE entry IN (5487, 48575, 48560, 16857, 48568, 48480, 48562);
+INSERT INTO spell_threat VALUES
+(5487, 0, 2.0735, 0), -- Bear Form
+(48575, 3474, 0, 0), -- Cower (Rank 6)
+(48560, 62, 0, 0), -- Demoralizing Roar (Rank 7) 
+(16857, 674, 0, 0), -- Faerie Fire (Feral)
+(48568, 515, 0.5, 0), -- Lacerate (Rank 3)
+(48480, 422, 0, 0), -- Maul (Rank 10)
+(48562, 0, 1.5, 0); -- Swipe (Rank 8)
