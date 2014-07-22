44efa491b831f0124135baf6ae82b67e3927a95b
Effec7 <dnlbunda@gmail.com>
2014-07-22 02:48:04 +0200

    Fix Avenging Wrath and Divine Shield (from now they shouldn't be usable at same time)
    Signed-off-by: Siley <r9.c@seznam.cz>

---
+++ sql/Deffender/world/Spells/avenging_wrath_and_divine_shield.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_linked_spell WHERE spell_trigger = 31884 AND spell_effect = 61988;
+INSERT INTO spell_linked_spell VALUES (31884, 61988, 2, 'Avenging Wrath + Divine Shield Exploit');
