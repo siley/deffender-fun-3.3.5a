c58f912808bf1942fc631690eaa1ba468871037f
Siley <r9.c@seznam.cz>
2014-08-08 17:03:39 +0200

    Revert "avenging wrath share 30 second CD with bubble"
    This reverts commit 2ea667c9329e55ebe8d5c61c0a4edcc320291011.
    
    Signed-off-by: Siley <r9.c@seznam.cz>

--- sql/Deffender/world/Spells/avenging_wrath_and_divine_shield.sql
+++ sql/Deffender/world/Spells/avenging_wrath_and_divine_shield.sql
@@ -1,2 +1,2 @@
-DELETE FROM `spell_linked_spell` WHERE (`spell_trigger`='31884') AND (`spell_effect`='61988');
-UPDATE `spell_dbc` SET `DurationIndex`='9' WHERE (`Id`='61988');
+DELETE FROM spell_linked_spell WHERE spell_trigger = 31884 AND spell_effect = 61988;
+INSERT INTO spell_linked_spell VALUES (31884, 61988, 2, 'Avenging Wrath + Divine Shield Exploit');
--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1560,6 +1560,18 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
     {
         switch (GetSpellInfo()->SpellFamilyName)
         {
+            case SPELLFAMILY_GENERIC:
+                switch (GetId())
+                {
+                    case 61987: // Avenging Wrath
+                        // Remove the immunity shield marker on Avenging Wrath removal if Forbearance is not present
+                        if (target->HasAura(61988) && !target->HasAura(25771))
+                            target->RemoveAura(61988);
+                        break;
+                    default:
+                        break;
+                }
+                break;
             case SPELLFAMILY_MAGE:
                 switch (GetId())
                 {
@@ -1744,6 +1756,11 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                 // Remove Vanish on stealth remove
                 if (GetId() == 1784)
                     target->RemoveAurasWithFamily(SPELLFAMILY_ROGUE, 0x0000800, 0, 0, target->GetGUID());
+                break;
+            case SPELLFAMILY_PALADIN:
+                // Remove the immunity shield marker on Forbearance removal if AW marker is not present
+                if (GetId() == 25771 && target->HasAura(61988) && !target->HasAura(61987))
+                    target->RemoveAura(61988);
                 break;
             case SPELLFAMILY_DEATHKNIGHT:
                 // Blood of the North
